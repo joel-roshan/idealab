454e664bbc7761a0a846549028534198f92a0987
joel-roshan <joeljosephroshan@gmail.com>
2023-01-09 18:56:16 +0530

    chumma

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className="header">
+            <p>
+                
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
