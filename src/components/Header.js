42579b443d92f7e712a9ff6f9b09c3866955f177
Joel K George <joelk9895@gmail.com>
2023-01-10 22:17:05 +0530

    Added carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,29 +6,29 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div>
-        <div className="header">
-          <div id="Navbar">
-            <div className="home hello">
-              <p id="hello">idealab MEC</p>
-            </div>
-            <div className="home">
-              <Link to ="/" className="navtext">Home</Link>
-              <Link to="/facilities" className="navtext nav-link">Facilities</Link> 
-              <Link to="/events" className="navtext nav-link">Events</Link>
-              <Link to="/team" className="navtext nav-link">Team</Link>
-              <Link to="/contact"className="navtext nav-link">Contact</Link>
-            </div>
-        <div className="buttons-nav home"> 
-          <Link to='/request' className='access'>Request Access</Link>
-        </div>
-        <div className="logocollege">
+      <div className="header">
+        <div id="Navbar">
+          <div className="home hello">
+            <p id="hello">idealab MEC</p>
+          </div>
+          <div className="home">
+            <Link to="/" className="navtext">Home</Link>
+            <Link to="/facilities" className="navtext nav-link">Facilities</Link>
+            <Link to="/events" className="navtext nav-link">Events</Link>
+            <Link to="/team" className="navtext nav-link">Team</Link>
+            <Link to="/contact" className="navtext nav-link">Contact</Link>
+          </div>
+          <div className="buttons-nav home">
+            <Link to='/request' className='access'>Request Access</Link>
+          </div>
+          <div className="logocollege">
             <a href="https://www.mec.ac.in" target="_blank">
-                <img src={logo} alt="MEC Logo" className="logos" />
-                   
-                        </a>
-                        </div>
-    </div>
+              <img src={logo} alt="MEC Logo" className="logos" />
+
+            </a>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Header from '../../components/Header'
+import './Home.css'
 
 const Home = () => {
   return (
     <div>
-        <Header />
+      <Header />
+      <div class="carousel">
+        <div class="announcement" data-tilt data-tilt-glare data-tilt-max-glare="0.8">
+          <h2 class="head-announce">from the lab</h2>
+          <p class="announce">announcement 1</p>
+          <p class="announce">announcement 2</p>
+          <p class="announce">announcement 3</p>
+          <p class="announce">announcement 4</p>
+        </div>
+        <div class="image"><img src={require('../asset/car.jpg')} alt="" class="car" /></div>
+      </div>
+      <hr class="carouselrule" />
+
     </div>
   )
 }
