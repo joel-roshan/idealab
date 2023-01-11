5ba74771fa485d2c74a4f89cc29d9804e9924a5f
Joel K George <joelk9895@gmail.com>
2023-01-11 16:14:03 +0530

    Fixed tilt issue

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,33 +8,33 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <div class="carousel">
-        <div class="announcement" data-tilt data-tilt-glare data-tilt-max-glare="0.8">
-          <h2 class="head-announce">from the lab</h2>
-          <p class="announce">announcement 1</p>
-          <p class="announce">announcement 2</p>
-          <p class="announce">announcement 3</p>
-          <p class="announce">announcement 4</p>
-        </div>
-        <div class="image"><img src={car} alt="" class="car" /></div>
+      <div className="carousel">
+            <div class="announcement" data-tilt>
+            <h2 className="head-announce">from the lab</h2>
+            <p className="announce">announcement 1</p>
+            <p className="announce">announcement 2</p>
+            <p className="announce">announcement 3</p>
+            <p className="announce">announcement 4</p>
+          </div>
+
+        <div className="image"><img src={car} alt="" className="car" /></div>
       </div>
-      <hr class="carouselrule" />
-      <div class="about" id ="about">
-        <div class="about-txt-hold">
-          <h2 class="about-txt">About idea Lab</h2>
+      <hr className="carouselrule" />
+      <div className="about" id="about">
+        <div className="about-txt-hold">
+          <h2 className="about-txt">About idea Lab</h2>
         </div>
 
-        <div class="about-content-image">
-          <div class="about-content-hold">
-            <p class="about-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis beatae dolore
+        <div className="about-content-image">
+          <div className="about-content-hold">
+            <p className="about-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis beatae dolore
               sint ab blanditiis repellat rerum tempore ipsa, quasi sapiente. Sed labore, illum distinctio ad
               earum
               inventore nobis dolor cumque!</p>
           </div>
-          <div class="about-image-hold"><img src={car} alt="about pic" class="about-image" /></div>
+          <div className="about-image-hold"><img src={car} alt="about pic" className="about-image" /></div>
         </div>
       </div>
-
     </div>
   )
 }
