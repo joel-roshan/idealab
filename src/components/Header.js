7b6e4476447d6437457005a6677e14d02f91a512
joel-roshan <joeljosephroshan@gmail.com>
2023-01-11 18:11:24 +0530

    navbar changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,32 +5,41 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
       <div className="header">
-        <div id="Navbar">
-          <div className="home hello">
+        
+          <div className="hello">
             <p id="hello">idealab MEC</p>
           </div>
-          <div className="home">
-            <Link to="/" className="navtext">Home</Link>
-            <Link to="/facilities" className="navtext nav-link">Facilities</Link>
-            <Link to="/events" className="navtext nav-link">Events</Link>
-            <Link to="/team" className="navtext nav-link">Team</Link>
-            <Link to="/contact" className="navtext nav-link">Contact</Link>
-          </div>
-          <div className="buttons-nav home">
-            <Link to='/request' className='access'>Request Access</Link>
-          </div>
-          <div className="logocollege">
-            <a href="https://www.mec.ac.in" target="_blank">
-              <img src={logo} alt="MEC Logo" className="logos" />
+
+          <div className="navbigdiv">
             
-            </a>
+            <div className="home">
+              <Link to="/" className="navtext">Home</Link>
+              <Link to="/facilities" className="navtext ">Facilities</Link>
+              <Link to="/events" className="navtext ">Events</Link>
+              <Link to="/team" className="navtext ">Team</Link>
+              <Link to="/contact" className="navtext ">Contact</Link>
+            </div>
+
+            <div className="nav2">
+                <div className="btnnav">
+                    <Link to='/request' className='access'>Request Access</Link>
+                  </div>
+
+                <div className="logocollege">
+                  <a href="https://www.mec.ac.in" target="_blank">
+                    <img src={logo} alt="MEC Logo" className="logos" />
+                  </a>
+                </div>
+
+            </div> 
+          
           </div>
-        </div>
+          
+        
       </div>
       
-    </div>
+      
   )
 }
 
