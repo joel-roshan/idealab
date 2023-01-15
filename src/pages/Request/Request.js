850d044e2ae0194ce86f274700bf371275c7df84
joel-roshan <joeljosephroshan@gmail.com>
2023-01-15 08:17:47 +0530

    request access page

--- src/pages/Request/Request.js
+++ src/pages/Request/Request.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import Header from '../../components/Header'
+import './Request.css'
 
 const Request = () => {
   return (
-    <div>
-        <Header />
-        <input type="text" placeholder='name' />
-        <input type="text"placeholder='email' />
-        <input type="text" />
+    <div><Header />
+      <div className="container">
+        <div className="message">
+          <h2 >
+            Request access to an equipment 
+          </h2>
+          <p>
+            Fill out these details
+          </p>
+        </div> 
+        <div className="input-container">
+          <div className="box"> 
+            <input className='input-box' type="text" placeholder='Name *' />
+          </div>
+          <div className="box"> 
+            <input className='input-box' type="text" placeholder='Phone No. *' />
+          </div> 
+          <div className="box"> 
+            <input className='input-box' type="text" placeholder='Email *' />
+          </div>
+          <div className="box"> 
+            <input className='input-box' type="text" placeholder='Class *' />
+          </div> 
+          <div className="box1"> 
+            <input className='sub-box' type="text" placeholder='Explain briefly what equipment you need an what you will be using it for *' />
+          </div>
+        </div>
+        <div className="sub-btn">
+          <button className='btnsub'>Submit</button>
+        </div>
+      </div>
     </div>
   )
 }
