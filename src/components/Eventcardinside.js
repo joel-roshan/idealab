4a17554facfd382b5f54dedd30523317357b0b66
joel-roshan <joeljosephroshan@gmail.com>
2023-03-23 06:01:30 +0530

    odal-modifications

--- src/components/Eventcardinside.js
+++ src/components/Eventcardinside.js
@@ -13,24 +13,113 @@ function Eventcardinside(props) {
   const toggleModal = () => {
     setModal(!modal);
   };
-  const [tab, setTab] = useState("about");
+  const [toggleState, setToggleState] = useState(1);
 
-  function selectTab(nextTab) {
-    setTab(nextTab);
-  }
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
 
   return (
     <div>
       {modal && (
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
-          <div className="modal-content">
-            <h2>{props.title}</h2>
-            <p>{props.desc}</p>
-            <button className="close-modal" onClick={toggleModal}>
-              <AiOutlineClose size={24} />
-            </button>
-          </div>
+            <div className="modal-box">
+              <div className="modal-content">
+                <div className="modal-logo" > 
+                 IDEALab
+                </div>
+                <h2>{props.title}</h2>
+                {/* <p>{props.desc}</p> */}
+                <div className="tabs-container">
+                  <div className="bloc-tabs">
+                    <button
+                      className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+                      onClick={() => toggleTab(1)}
+                    >
+                      Tab 1
+                    </button>
+                    <button
+                      className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+                      onClick={() => toggleTab(2)}
+                    >
+                      Tab 2
+                    </button>
+                    <button
+                      className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+                      onClick={() => toggleTab(3)}
+                    >
+                      Tab 3
+                    </button>
+                  </div>
+
+                  <div className="content-tabs">
+                    <div
+                      className={toggleState === 1 ? "content  active-content" : "content"}
+                    >
+                      <h2>About</h2>
+                      <hr />
+                      <p>
+                        {props.about}
+                      </p>
+                      <div className="ven-date-time">
+                        <div className="ven">
+                          <p>
+                            Venue: {props.location}
+                          </p>
+                          
+                        </div>
+                        <div className="ven">
+                          <p>
+                            Date: {props.date}
+                          </p>
+                          
+                        </div>
+                        <div className="ven">
+                          <p>
+                            Time: {props.time}
+                          </p>
+                          
+                        </div>
+                      </div>
+                    </div>
+
+                    <div
+                      className={toggleState === 2 ? "content  active-content" : "content"}
+                    >
+                      <h2>Registration</h2>
+                      <hr />
+                      <p>
+                        {props.reg}
+                      </p>
+                      <div className="reg-btn-wrap">
+                      <a href={props.reglink} target='_blank'><button className="reg-btn">Register</button></a>
+                      </div>
+                      
+
+                    </div>
+
+                    <div
+                      className={toggleState === 3 ? "content  active-content" : "content"}
+                    >
+                      <h2>Contact</h2>
+                      <hr />
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
+                        nostrum rerum laudantium totam unde adipisci incidunt modi alias!
+                        Accusamus in quia odit aspernatur provident et ad vel distinctio
+                        recusandae totam quidem repudiandae omnis veritatis nostrum
+                        laboriosam architecto optio rem, dignissimos voluptatum beatae
+                        aperiam voluptatem atque. Beatae rerum dolores sunt.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <button className="close-modal " onClick={toggleModal}>
+                 {/* <AiOutlineClose size={24} /> */}X
+                </button>
+              </div>
+            </div>  
         </div>
       )}
 
@@ -50,10 +139,10 @@ function Eventcardinside(props) {
               <p>{props.date}</p>
             </div>
             <div className="location">
-              <a href={props.locationlink} target="_blank">
-                <CiLocationOn size={24} />
+              <a href={props.locationlink} target="_blank" className="link">
+                <CiLocationOn size={24} /><p>{props.location}</p>
               </a>
-              <p>{props.location}</p>
+              {/* <p>{props.location}</p> */}
             </div>
           </div>
           <div className="status-time">
@@ -61,7 +150,7 @@ function Eventcardinside(props) {
               <p>{props.status}</p>
             </div>
             <div className="time">
-              <p>{props.time}</p>
+              {/* <p>{props.time}</p> */}
             </div>
             <div className="btn-info">
               <button onClick={toggleModal} className="more-btn">
--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -8,7 +8,6 @@ import poster from "../../asset/poster.jpeg";
 import Footer from "../../components/Footer";
 import EventcardInsidelist from "../../components/EventcardInsidelist";
 import TabButton from "./Tabs";
-
 const Events = () => {
   const [events, setEvents] = useState([
     {
@@ -52,9 +51,13 @@ const Events = () => {
             quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
             placeat tempora vitae enim incidunt porro fuga ea."
             date="19-01-2003"
-            location="Mec"
+            location="MEC"
             locationlink="https://www.google.com/maps/place/Model+Engineering+College+Road,+Karimakkad,+Thrikkakara,+Edappally,+Kochi,+Kerala+682021/@10.0283637,76.3263237,17z/data=!4m12!1m6!3m5!1s0x3b080c5006491601:0xcf23e14245d4694d!2sGovernment+Model+Engineering+College!8m2!3d10.0283637!4d76.3285124!3m4!1s0x3b080c5074bffe33:0x1747d298a33d657a!8m2!3d10.0283525!4d76.328587"
             pic={poster}
+            about= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus amet dignissimos laudantium quibusdam, impedit facilis doloremque atque nostrum debitis? Porro dolore fugit aspernatur, ullam dolores explicabo exercitationem esse in repudiandae."
+            reg="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus sapiente doloremque libero harum dignissimos laborum soluta, facilis eum deserunt et saepe perferendis enim ipsam animi nisi magnam aspernatur dicta! Magni!"
+            
+            time="9:30 AM"
           />
           <Eventcardinside
             title="pls work"
@@ -66,7 +69,7 @@ const Events = () => {
             quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
             placeat tempora vitae enim incidunt porro fuga ea. smol change tho"
             date="19-01-2003"
-            location="Mec"
+            location="MEC"
             locationlink="https://www.google.com/maps/place/Model+Engineering+College+Road,+Karimakkad,+Thrikkakara,+Edappally,+Kochi,+Kerala+682021/@10.0283637,76.3263237,17z/data=!4m12!1m6!3m5!1s0x3b080c5006491601:0xcf23e14245d4694d!2sGovernment+Model+Engineering+College!8m2!3d10.0283637!4d76.3285124!3m4!1s0x3b080c5074bffe33:0x1747d298a33d657a!8m2!3d10.0283525!4d76.328587"
             pic={poster}
           />
